5251aed3af99ac2676e9f14de63fc1fa1d9d7519
Elizaveta Bezyakina <e-bezyakina@yandex-team.ru>
2020-09-11 23:56:14 +0300

    review fixes

--- homework.py
+++ homework.py
@@ -15,66 +15,86 @@ logging.basicConfig(
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+BOT_TIME_SLEEP = os.getenv("BOT_TIME_SLEEP")
 
 PRACTICUM_BASE_URL = "https://praktikum.yandex.ru/api/user_api"
+HOMEWORK_STATUSES_ENDPOINT = "homework_statuses"
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = homework["homework_name"]
-    homework_status = homework["status"]
-    if homework_status == "rejected":
-        logging.info("Получен статус домашней работы - rejected")
-        verdict = "К сожалению в работе нашлись ошибки."
-    else:
-        logging.info("Получен статус домашней работы - aproved")
-        verdict = (
-            "Ревьюеру всё понравилось, можно приступать к следующему " "уроку."
-        )
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    if homework.keys() & {"homework_name", "status"}:
+
+        homework_name = homework["homework_name"]
+        homework_status = homework["status"]
+
+        if homework_status == "rejected":
+            logging.info("Получен статус домашней работы - rejected")
+            verdict = "К сожалению в работе нашлись ошибки."
+
+        elif homework_status == "approved":
+            logging.info("Получен статус домашней работы - approved")
+            verdict = (
+                "Ревьюеру всё понравилось, можно приступать к следующему "
+                "уроку."
+            )
+
+        else:
+            print("Неизвестный статус домашнего задания")
+
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+
+    print("Произошла ошибка при запросе")
 
 
 def get_homework_statuses(current_timestamp):
     headers = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
-    params = {"from_date": current_timestamp}
-    try:
-        homework_statuses = requests.get(
-            f"{PRACTICUM_BASE_URL}/homework_statuses/",
-            headers=headers,
-            params=params,
-        )
-        logging.info("Получен ответ от API практикума")
-    except Exception as e:
-        logging.error(
-            f"Не удалось получить ответ от API практикума, ошибка: {e}"
-        )
-        print(f"Не удалось получить ответ от API практикума, ошибка: {e}")
-    return homework_statuses.json()
+
+    if isinstance(current_timestamp, (int, str)):
+        params = {"from_date": int(current_timestamp)}
+
+        try:
+            homework_statuses = requests.get(
+                f"{PRACTICUM_BASE_URL}/homework_statuses/",
+                headers=headers,
+                params=params,
+            )
+            logging.info("Получен ответ от API практикума")
+            return homework_statuses.json()
+
+        # Я правильно понимаю, что нужно описывать множество исключений,
+        # например, как тут https://stackoverflow.com/a/47007419 ?
+        except Exception as e:
+            print(f"Не удалось получить ответ от API практикума, ошибка: {e}")
+
+    else:
+        print("Полученый current_timestamp должен имметь тип int или str")
 
 
 def send_message(message):
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
     return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
     logging.info("Started")
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())
+
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
+
             if new_homework.get("homeworks"):
                 send_message(
                     parse_homework_status(new_homework.get("homeworks")[0])
                 )
-                logging.debug("Сообщение было успешно отправлено в telegram")
-            current_timestamp = new_homework.get(
-                "current_date"
-            )  # обновить timestamp
-            time.sleep(1200)  # опрашивать раз в двадцать минут
+                logging.info("Сообщение было успешно отправлено в telegram")
+            current_timestamp = new_homework.get("current_date")
+
+            time.sleep(int(BOT_TIME_SLEEP))
 
         except Exception as e:
             logging.error(f"Бот упал с ошибкой: {e}")
-            print(f"Бот упал с ошибкой: {e}")
             time.sleep(5)
             continue
 
