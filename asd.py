52209054da31d9a615ec976b6f65e5adea08fb25
Elizaveta Bezyakina <e-bezyakina@yandex-team.ru>
2020-09-12 23:08:22 +0300

    review fixes

---
+++ asd.py
@@ -0,0 +1,16 @@
+# a = {2: '2'}
+# a = {1: '1', 2: '2', 3: '3'}
+# if a.keys() & {1} and a.keys() & {2}:
+#     print("Contain keys")
+# else:
+#     print("Not contain keys")
+# a = {2: '2'}
+# if a.keys() == {1, 2}:
+#     print("Contain keys")
+# else:
+#     print("Not contain keys")
+a = {3: "3"}
+if 1 and 2 not in a.keys():
+    print("Not contain keys")
+else:
+    print("Contain keys")
--- homework.py
+++ homework.py
@@ -24,52 +24,64 @@ bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    if homework.keys() & {"homework_name", "status"}:
+    if "homework_name" and "status" not in homework.keys():
+        logging.error("Произошла ошибка при запросе")
+        return (
+            f"Произошла ошибка при запросе к API практикума, повторный "
+            f"запрос через {int(BOT_TIME_SLEEP) // 60} минут"
+        )
 
-        homework_name = homework["homework_name"]
-        homework_status = homework["status"]
+    homework_name = homework["homework_name"]
+    homework_status = homework["status"]
 
-        if homework_status == "rejected":
-            logging.info("Получен статус домашней работы - rejected")
-            verdict = "К сожалению в работе нашлись ошибки."
+    homework_statuses = {
+        "rejected": "К сожалению в работе нашлись ошибки.",
+        "approved": "Ревьюеру всё понравилось, можно "
+        "приступать к следующему уроку.",
+    }
 
-        elif homework_status == "approved":
-            logging.info("Получен статус домашней работы - approved")
-            verdict = (
-                "Ревьюеру всё понравилось, можно приступать к следующему "
-                "уроку."
-            )
+    if homework_status not in homework_statuses.keys():
+        logging.error("Неизвестный статус домашнего задания")
+        return "Неизвестный статус домашнего задания"
 
-        else:
-            print("Неизвестный статус домашнего задания")
+    logging.info(f"Получен статус домашней работы - {homework_status}")
+    verdict = homework_statuses[homework_status]
 
-        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
-
-    print("Произошла ошибка при запросе")
+    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
     headers = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
-    if isinstance(current_timestamp, (int, str)):
-        params = {"from_date": int(current_timestamp)}
-
-        try:
-            homework_statuses = requests.get(
-                f"{PRACTICUM_BASE_URL}/homework_statuses/",
-                headers=headers,
-                params=params,
-            )
-            logging.info("Получен ответ от API практикума")
-            return homework_statuses.json()
-
-        # Я правильно понимаю, что нужно описывать множество исключений,
-        # например, как тут https://stackoverflow.com/a/47007419 ?
-        except Exception as e:
-            print(f"Не удалось получить ответ от API практикума, ошибка: {e}")
-
-    else:
+    if not isinstance(current_timestamp, (int, str)):
+        logging.error(
+            "Полученый current_timestamp должен имметь тип int или " "str"
+        )
         print("Полученый current_timestamp должен имметь тип int или str")
+
+    params = {"from_date": int(current_timestamp)}
+
+    try:
+        homework_statuses = requests.get(
+            f"{PRACTICUM_BASE_URL}/{HOMEWORK_STATUSES_ENDPOINT}/",
+            headers=headers,
+            params=params,
+        )
+        logging.info("Получен ответ от API практикума")
+        return homework_statuses.json()
+
+    except requests.exceptions.HTTPError as errh:
+        logging.error("Http Error: ", errh)
+        print("Http Error: ", errh)
+    except requests.exceptions.ConnectionError as errc:
+        logging.error("Error Connecting: ", errc)
+        print("Error Connecting: ", errc)
+    except requests.exceptions.Timeout as errt:
+        logging.error("Timeout Error: ", errt)
+        print("Timeout Error: ", errt)
+    except requests.exceptions.RequestException as err:
+        logging.error("OOps: Something Else ", err)
+        print("Не удалось получить ответ от API практикума, ошибка ", err)
 
 
 def send_message(message):
@@ -89,8 +101,8 @@ def main():
                     parse_homework_status(new_homework.get("homeworks")[0])
                 )
                 logging.info("Сообщение было успешно отправлено в telegram")
-            current_timestamp = new_homework.get("current_date")
 
+            current_timestamp = new_homework.get("current_date")
             time.sleep(int(BOT_TIME_SLEEP))
 
         except Exception as e:
